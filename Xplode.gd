8330c84ba3881da5b6aeca0e4b344f4aa965b0a1
borb <perttu.tuovinen@buutti.com>
2023-04-30 14:09:33 +0300

    add explosion functions, comment function calls to fix crash

--- Xplode.gd
+++ Xplode.gd
@@ -15,16 +15,24 @@ func _ready():
 #func _process(delta):
 #    pass
 
+func create_explosion(body):
+    var xplosion = Xplode.instance()
+    xplosion.global_position = body.global_position
+    xplosion.sender = sender
+    get_parent().add_child(xplosion)
+    
+func create_scorepop(body, score):
+    var scorepop = ScorePop.instance()
+    scorepop.global_position = body.global_position
+    scorepop.txt = str(score) + "p"
+    get_parent().add_child(scorepop)
 
 func _on_Xplode_body_entered(body):
     var score = 5 + randi()%10 + randi()%10
     if body.is_in_group("XplodePeg") or body.is_in_group("BombPeg"):
         if body.global_position == global_position:
             return
-        var xplosion = Xplode.instance()
-        xplosion.global_position = body.global_position
-        xplosion.sender = sender
-        get_parent().add_child(xplosion)
+        # create_explosion(body)
         score *= 2
     
     if body.is_in_group("BadPeg"):
@@ -36,10 +44,7 @@ func _on_Xplode_body_entered(body):
     
     
     if body.is_in_group("RemovePeg"):
-        var scorepop = ScorePop.instance()
-        scorepop.global_position = body.global_position
-        scorepop.txt = str(score) + "p"
-        get_parent().add_child(scorepop)
+        # create_scorepop(body, score)
         Global.add_score(sender, score)
         
         body.queue_free()
