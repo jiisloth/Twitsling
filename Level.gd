600c1c36f48ba573529d37e833df37d78798b39c
borbware <perttu.tuovinen@gmail.com>
2023-05-03 11:22:14 +0300

    edit breakout

--- Level.gd
+++ Level.gd
@@ -19,124 +19,124 @@ var launchpos
 var lvlindex = 0
 
 func _ready():
-    $Levels.hide()
-    launchpos = $BG/Viuhti/Launchpos.global_position
-    lvl = $Levels.get_child(lvlindex)
-    set_pegs()
+	$Levels.hide()
+	launchpos = $BG/Viuhti/Launchpos.global_position
+	lvl = $Levels.get_child(lvlindex)
+	set_pegs()
 
 func set_level(add):
-    lvlindex += add
-    if lvlindex < 0:
-        lvlindex = $Levels.get_child_count()-1
-    if lvlindex >= $Levels.get_child_count():
-        lvlindex = 0
-    lvl = $Levels.get_child(lvlindex)
-    set_pegs()
+	lvlindex += add
+	if lvlindex < 0:
+		lvlindex = $Levels.get_child_count()-1
+	if lvlindex >= $Levels.get_child_count():
+		lvlindex = 0
+	lvl = $Levels.get_child(lvlindex)
+	set_pegs()
 
   
 func rewind():
-    set_pegs(false)
-    
+	set_pegs(false)
+	
 func set_pegs(initial=true):
-    if initial:
-        $Hodl.clear()
-        var stones = get_tree().get_nodes_in_group("Stone")
-        for stone in stones:
-            Global.set_inactive(stone.sender)
-            stone.queue_free()
-        var pegs = get_tree().get_nodes_in_group("Peg")
-        for peg in pegs:
-            peg.queue_free()
-            
-    var tiles = lvl.get_used_cells()
-    for coord in tiles:
-        var cell = lvl.get_cell(coord.x, coord.y)
-        add_peg(coord, cell, initial)
-        if initial:
-            $Hodl.set_cell(coord.x, coord.y, 2)
-    if initial:
-        lvl.hide()
-        
+	if initial:
+		$Hodl.clear()
+		var stones = get_tree().get_nodes_in_group("Stone")
+		for stone in stones:
+			Global.set_inactive(stone.sender)
+			stone.queue_free()
+		var pegs = get_tree().get_nodes_in_group("Peg")
+		for peg in pegs:
+			peg.queue_free()
+			
+	var tiles = lvl.get_used_cells()
+	for coord in tiles:
+		var cell = lvl.get_cell(coord.x, coord.y)
+		add_peg(coord, cell, initial)
+		if initial:
+			$Hodl.set_cell(coord.x, coord.y, 2)
+	if initial:
+		lvl.hide()
+		
 
 func add_peg(coord, variant, initial):
-    var pegpos = coord * 8 * 2 + Vector2(16,16)
-    if not initial:
-        var stones = get_tree().get_nodes_in_group("Stone")
-        for stone in stones:
-            if (stone.position - pegpos).length() < 32:
-                return
-        var pegs = get_tree().get_nodes_in_group("Peg")
-        for peg in pegs:
-            if (peg.position - pegpos).length() < 5:
-                return
-    var peg
-    match variant:
-        0:
-            return
-        1:
-            peg = Peg.instance()
-        2:
-            var r = randi()%100
-            peg = PegNormal.instance()
-            if r < 1:
-                peg = PegRewind.instance()
-            elif r < 11:
-                peg = PegBomb.instance()
-            elif r < 21:
-                peg = PegSticky.instance()
-            elif r < 31:
-                peg = PegBounceRemove.instance()
-        3:
-            peg = PegNormal.instance()
-        4:
-            peg = PegSticky.instance()
-        5:
-            peg = PegRewind.instance()
-        6:
-            peg = PegBomb.instance()
-        7:
-            peg = PegXplode.instance()
-        8:
-            peg = PegBad.instance()
-        9:
-            peg = PegBounce.instance()
-        10:
-            peg = PegBounceRemove.instance()
-    if peg != null:
-        peg.position = pegpos
-        add_child(peg)
-    
+	var pegpos = coord * 8 * 2 + Vector2(16,16)
+	if not initial:
+		var stones = get_tree().get_nodes_in_group("Stone")
+		for stone in stones:
+			if (stone.position - pegpos).length() < 32:
+				return
+		var pegs = get_tree().get_nodes_in_group("Peg")
+		for peg in pegs:
+			if (peg.position - pegpos).length() < 5:
+				return
+	var peg
+	match variant:
+		0:
+			return
+		1:
+			peg = Peg.instance()
+		2:
+			var r = randi()%100
+			peg = PegNormal.instance()
+			if r < 1:
+				peg = PegRewind.instance()
+			elif r < 11:
+				peg = PegBomb.instance()
+			elif r < 21:
+				peg = PegSticky.instance()
+			elif r < 31:
+				peg = PegBounceRemove.instance()
+		3:
+			peg = PegNormal.instance()
+		4:
+			peg = PegSticky.instance()
+		5:
+			peg = PegRewind.instance()
+		6:
+			peg = PegBomb.instance()
+		7:
+			peg = PegXplode.instance()
+		8:
+			peg = PegBad.instance()
+		9:
+			peg = PegBounce.instance()
+		10:
+			peg = PegBounceRemove.instance()
+	if peg != null:
+		peg.position = pegpos
+		add_child(peg)
+	
 
 func shoot(sender, dir, power):
-    var stone = Stone.instance()
-    stone.sender = sender
-    stone.global_position = launchpos
-    stone.init_d = dir
-    stone.init_p = power
-    add_child(stone)
+	var stone = Stone.instance()
+	stone.sender = sender
+	stone.global_position = launchpos
+	stone.init_d = dir
+	stone.init_p = power
+	add_child(stone)
 
 
 func _on_KillZone_body_entered(body):
-    if body.is_in_group("Stone"):
-        Global.set_inactive(body.sender)
-        body.queue_free()
+	if body.is_in_group("Stone"):
+		Global.set_inactive(body.sender)
+		body.queue_free()
 
 
 func create_explosion(pos, sender):
-    var xplosion = Xplode.instance()
-    xplosion.global_position = pos
-    xplosion.sender = sender
-    add_child(xplosion)
-    
-    
+	var xplosion = Xplode.instance()
+	xplosion.global_position = pos
+	xplosion.sender = sender
+	add_child(xplosion)
+	
+	
 func create_pegxplode(pos):
-    var pegxplosion = PegXplode.instance()
-    pegxplosion.global_position = pos
-    add_child(pegxplosion)
-    
-    
+	var pegxplosion = PegXplode.instance()
+	pegxplosion.global_position = pos
+	add_child(pegxplosion)
+	
+	
 func create_scorepop(pos, score):
-    var scorepop = ScorePop.instance()
-    scorepop.global_position = pos
-    scorepop.txt = str(score) + "p"
-    add_child(scorepop)
+	var scorepop = ScorePop.instance()
+	scorepop.global_position = pos
+	scorepop.txt = str(score) + "p"
+	add_child(scorepop)
--- Main.gd
+++ Main.gd
@@ -3,65 +3,65 @@ extends Node2D
 var adminshoot = false
 
 func _ready():
-    randomize()
-    set_level_name()
-    
+	randomize()
+	set_level_name()
+	
 
 func shoot(sender, dir, power, admin=false):
-    if not Global.paused:
-        if not Global.check_active(sender):
-            $Level.shoot(sender, dir, power)
-            if admin and Config.allow_admin_spam:
-                Global.set_inactive(sender)
-            else:
-                Global.set_active(sender)
+	if not Global.paused:
+		if not Global.check_active(sender):
+			$Level.shoot(sender, dir, power)
+			if admin and Config.allow_admin_spam:
+				Global.set_inactive(sender)
+			else:
+				Global.set_active(sender)
 
 
 func _process(delta):
-    if Input.is_action_just_pressed("Pause"):
-        if Global.paused:
-            Global.paused = false
-            $PauseScreen.hide()
-        else:
-            Global.paused = true
-            adminshoot = false
-            $PauseScreen.show()
-    if Input.is_action_just_pressed("shoot"):
-        
-        if not Global.paused:
-            if adminshoot:
-                var mouse = get_viewport().get_mouse_position()
-                var diff = $Level.launchpos - mouse
-                var dir = rad2deg(diff.angle())-180
-                if dir <= -180:
-                    dir += 360
-                var power = min(diff.length(), 100)
-                print("Admin shot: d: ", -dir, " p: ", power)
-                shoot("<!> Admin", dir, power, true)
-            else:
-                adminshoot = true
-            
+	if Input.is_action_just_pressed("Pause"):
+		if Global.paused:
+			Global.paused = false
+			$PauseScreen.hide()
+		else:
+			Global.paused = true
+			adminshoot = false
+			$PauseScreen.show()
+	if Input.is_action_just_pressed("shoot"):
+		
+		if not Global.paused:
+			if adminshoot:
+				var mouse = get_viewport().get_mouse_position()
+				var diff = $Level.launchpos - mouse
+				var dir = rad2deg(diff.angle())-180
+				if dir <= -180:
+					dir += 360
+				var power = min(diff.length(), 100)
+				print("Admin shot: d: ", -dir, " p: ", power)
+				shoot("<!> Admin", dir, power, true)
+			else:
+				adminshoot = true
+			
 func set_level_name():
-    var lvl = $Level.lvl.name
-    lvl = Array(lvl.split("_"))
-    var creator = lvl.pop_back()
-    var lvlname = " ".join(lvl)
-    $UI2/LVLinfo.text = lvlname + "\n- " + creator
-               
+	var lvl = $Level.lvl.name
+	lvl = Array(lvl.split("_"))
+	var creator = lvl.pop_back()
+	var lvlname = " ".join(lvl)
+	$UI2/LVLinfo.text = lvlname + "\n- " + creator
+			   
 
 func _on_Continue_gui_input(event):
-    if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
-        Global.paused = false
-        $PauseScreen.hide()
+	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
+		Global.paused = false
+		$PauseScreen.hide()
 
 
 func _on_Previous_gui_input(event):
-    if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
-        $Level.set_level(-1)
-        set_level_name()
+	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
+		$Level.set_level(-1)
+		set_level_name()
 
 
 func _on_Next_gui_input(event):
-    if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
-        $Level.set_level(1)
-        set_level_name()
+	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
+		$Level.set_level(1)
+		set_level_name()
